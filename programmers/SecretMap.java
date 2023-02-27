59085084ba363af8e0fe4a874d65841fae93f352
koesnam <76582376+10kseok@users.noreply.github.com>
2023-02-28 08:04:44 +0900

    BOJ 10828 스택 문제 풀이

--- programmers/SecretMap.java
+++ programmers/SecretMap.java
@@ -7,6 +7,9 @@ public class SecretMap {
     
     public static String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = {};
+        // logic
+
+
         return answer;
     }
 
@@ -21,19 +24,20 @@ public class SecretMap {
     }
 
     public static void main(String[] args) {
-        int testN1 = 5;
-        int[] testArr1_1 = new int[] {9, 20, 28, 18, 11};
-        int[] testArr1_2 = new int[] {30, 1, 21, 17, 28};
-        String[] testResult1 = new String[] {"#####", "# # #",  "### #", "# ##", "#####"};
+        // int testN1 = 5;
+        // int[] testArr1_1 = new int[] {9, 20, 28, 18, 11};
+        // int[] testArr1_2 = new int[] {30, 1, 21, 17, 28};
+        // String[] testResult1 = new String[] {"#####", "# # #",  "### #", "# ##", "#####"};
         
-        System.out.println(validate(testResult1, solution(testN1, testArr1_1, testArr1_2)));
+        // System.out.println(validate(testResult1, solution(testN1, testArr1_1, testArr1_2)));
 
 
-        int testN2 = 6;
-        int[] testArr2_1 = new int[] {46, 33, 33 ,22, 31, 50};
-        int[] testArr2_2 = new int[] {27 ,56, 19, 14, 14, 10};
-        String[] testResult2 = new String[] {"######", "### #", "## ##", " #### ", " #####", "### # "};
+        // int testN2 = 6;
+        // int[] testArr2_1 = new int[] {46, 33, 33 ,22, 31, 50};
+        // int[] testArr2_2 = new int[] {27 ,56, 19, 14, 14, 10};
+        // String[] testResult2 = new String[] {"######", "### #", "## ##", " #### ", " #####", "### # "};
 
-        System.out.println(validate(testResult2, solution(testN2, testArr2_1, testArr2_2)));
+        // System.out.println(validate(testResult2, solution(testN2, testArr2_1, testArr2_2)));
+        System.out.println(47 | 20);
     }
 }
