6c4abf3b8177c82154475c59850a59553550a09a
koesnam <76582376+10kseok@users.noreply.github.com>
2023-12-14 16:24:14 +0900

    프로그래머스 Lv.3 단어변환 (1차 개선)
    - 단계를 측정하려고 뒀던 변수를 Node 클래스에 내장하면서 Queue에 List를 넣지 않아도 됐다.
    - 방문 기록을 측정하여 중복 검사를 없앴다.
    - 로직을 책임에 따라 클래스로 분리하면서 가독성을 높아졌다.

--- solutions/src/programmers/WordConversion.java
+++ solutions/src/programmers/WordConversion.java
@@ -14,33 +14,92 @@ import java.util.Queue;
  * @since 2023.12.14
  */
 public class WordConversion {
+//    public int solution(String begin, String target, String[] words) {
+//        if (Arrays.stream(words)
+//                .noneMatch(word -> word.equals(target))) return 0;
+//
+//        int step = 0;
+//        int n = begin.length();
+//
+//        Queue<List<String>> searchQ = new LinkedList<>();
+//        searchQ.add(List.of(begin));
+//
+//        while (!searchQ.isEmpty()) {
+//            List<String> similarities = new LinkedList<>();
+//            List<String> available = searchQ.poll();
+//            for (String curWord : available) {
+//                if (curWord.equals(target)) return step;
+//                for (String word : words) {
+//                    int count = 0;
+//                    for (int i = 0; i < n; i++) {
+//                        if (curWord.charAt(i) == word.charAt(i)) count++;
+//                    }
+//                    if (count == n - 1) similarities.add(word);
+//                }
+//            }
+//            searchQ.add(similarities);
+//            step++;
+//        }
+//        return 0;
+//    }
+
+    // 1차 개선
     public int solution(String begin, String target, String[] words) {
         if (Arrays.stream(words)
                 .noneMatch(word -> word.equals(target))) return 0;
 
-        int step = 0;
-        int n = begin.length();
+        Logger logger = new Logger(words.length);
+        Queue<Node> tree = new LinkedList<>();
+        tree.offer(new Node(0, begin));
 
-        Queue<List<String>> searchQ = new LinkedList<>();
-        searchQ.add(List.of(begin));
-
-        while (!searchQ.isEmpty()) {
-            List<String> similarities = new LinkedList<>();
-            List<String> available = searchQ.poll();
-            for (String curWord : available) {
-                if (curWord.equals(target)) return step;
-                for (String word : words) {
-                    int count = 0;
-                    for (int i = 0; i < n; i++) {
-                        if (curWord.charAt(i) == word.charAt(i)) count++;
-                    }
-                    if (count == n - 1) similarities.add(word);
+        while (!tree.isEmpty()) {
+            Node curNode = tree.poll();
+            if (curNode.data.equals(target)) return curNode.level;
+            for (int i = 0; i < words.length; i++) {
+                if (logger.hasLogAt(i)) continue;
+                if (SimilarityValidator.validate(curNode.data, words[i])) {
+                    tree.offer(new Node(curNode.level + 1, words[i]));
+                    logger.log(i);
                 }
             }
-            searchQ.add(similarities);
-            step++;
         }
         return 0;
+    }
+
+    static class Logger {
+        private final boolean[] logs;
+
+        public Logger(int n) {
+            this.logs = new boolean[n];
+        }
+
+        void log(int i) {
+            this.logs[i] = true;
+        }
+
+        boolean hasLogAt(int i) {
+            return this.logs[i];
+        }
+    }
+
+    static class SimilarityValidator {
+        static boolean validate(String a, String b) {
+            int count = 0;
+            for (int i = 0; i < a.length(); i++) {
+                if (a.charAt(i) == b.charAt(i)) count++;
+            }
+            return count == a.length() - 1;
+        }
+    }
+
+    static class Node {
+        private int level;
+        private String data;
+
+        public Node(int level, String data) {
+            this.level = level;
+            this.data = data;
+        }
     }
 
     public static void main(String[] args) {
