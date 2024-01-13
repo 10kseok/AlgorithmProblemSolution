3b8ae26561846a77c52007bf8d1ee18786b65ec5
추만석 <koesnam.dev@gmail.com>
2024-01-13 16:24:27 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 308 ms, Memory: 112440 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -10,11 +10,10 @@ prime_table[0] = 0
 for i in range(2, max_T_idx):
     if prime_table[i] == 0:
         continue
-    for j in range(i, max_T_idx, i):
+    for j in range(i * 2, max_T_idx, i):
         if prime_table[j] == 0:
             continue
-        if i != j:
-            prime_table[j] = 0
+        prime_table[j] = 0
 # 소수 합 찾기
 for idx, t in enumerate(T):
     for num in range(2, t + 1):
