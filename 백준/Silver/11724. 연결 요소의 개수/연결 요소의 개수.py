b8a3de76bd94f0ab2686c807653357fe2b118b3b
추만석 <koesnam.dev@gmail.com>
2024-01-23 19:13:28 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 784 ms, Memory: 65220 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,26 +1,26 @@
 import sys
+
+sys.setrecursionlimit(10 ** 6)
     
-# 각 자료구조별 특징
-# 인접 행렬 -> 연결된 노드들을 찾아서 계산해줘야함 -> 행렬로 인한 공간 복잡도 상승
-# 인접 리스트 -> 연결된 노드들을 번호순으로 계속 정렬해줘야함 -> 코드가 훨씬 적어짐.
-
 N, M = map(int, input().split())
-graph = [[] * (N + 1) for _ in range(N + 1)]
-parent = [i for i in range(0, N + 1)]
-
-def find(x):
-    if parent[x] != x:
-        parent[x] = find(parent[x])
-    return parent[x]
-
+graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+count = 0
 for _ in range(M):
     v1, v2 = map(int, sys.stdin.readline().split())
-    smaller = find(v1)
-    larger = find(v2)   
-    parent[max(smaller, larger)] = min(smaller, larger)
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+def dfs(start, subset):
+    visited[start] = True
+    subset.add(start)
+    for next in graph[start]:
+        if not visited[next]:
+            dfs(next, subset)
+    return subset
 
 for i in range(1, N + 1):
-    find(i)
+    if not visited[i] and len(dfs(i, set())) > 0:
+        count += 1
 
-print(len(set(parent[1:])))
-
+print(count)
