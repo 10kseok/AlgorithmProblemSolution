6a38cadbf948c4d75a6c75eeceb048648ee993f0
추만석 <koesnam.dev@gmail.com>
2024-01-27 11:05:22 +0900

    [Gold V] Title: 평범한 배낭, Time: 228 ms, Memory: 34608 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -15,7 +15,7 @@ def knapsack(MAX_W, weight_value, n):
         weight =  weight_value[i][0]
         value  =  weight_value[i][1]
         for w in range(1, MAX_W + 1):
-            if (w - weight) >= 0:
+            if w >= weight:
                 dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weight] + value)
             else:
                 dp[i][w] = dp[i - 1][w]
@@ -33,5 +33,23 @@ def knapsack_v2(MAX_W, weight_value, n):
     
     return dp[MAX_W]
 
+def knapsack_v3(MAX_W, weight_value):
+    # value : weight
+    max_value_bag = {0 : 0}
+    max_first_wv = sorted(weight_value, reverse=True)
+
+    for w, v in max_first_wv:
+        temp_bag = {}
+        for _v, _w in max_value_bag.items():
+            w_if_added = _w + w
+            v_if_added = _v + v
+            if w_if_added < max_value_bag.get(v_if_added, MAX_W + 1):
+                temp_bag[v_if_added] = w_if_added
+
+        max_value_bag.update(temp_bag)
+    return max(max_value_bag.keys())
+
+
 # print(knapsack(MAX_W, weight_value, N))
-print(knapsack_v2(MAX_W, weight_value, N))
+# print(knapsack_v2(MAX_W, weight_value, N))
+print(knapsack_v3(MAX_W, weight_value))
