0d11c755648be66afc24471c20dd5f9e3a91bcf7
추만석 <koesnam.dev@gmail.com>
2024-01-27 10:31:36 +0900

    [Gold V] Title: 평범한 배낭, Time: 2148 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -21,4 +21,17 @@ def knapsack(MAX_W, weight_value, n):
                 dp[i][w] = dp[i - 1][w]
     return dp[n][MAX_W]
 
-print(knapsack(MAX_W, weight_value, N))
+def knapsack_v2(MAX_W, weight_value, n):
+    dp = [0 for _ in range(MAX_W + 1)]
+
+    for i in range(1, n + 1):
+        weight =  weight_value[i][0]
+        value  =  weight_value[i][1]
+        for w in range(MAX_W, 0, -1):
+            if weight <= w:
+                dp[w] = max(dp[w], dp[w - weight] + value)
+    
+    return dp[MAX_W]
+
+# print(knapsack(MAX_W, weight_value, N))
+print(knapsack_v2(MAX_W, weight_value, N))
