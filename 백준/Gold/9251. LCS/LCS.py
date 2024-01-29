04d2fd496754bc14577a9e58dc67787c805d7be6
추만석 <koesnam.dev@gmail.com>
2024-01-29 17:35:20 +0900

    [Gold V] Title: LCS, Time: 224 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,6 +1,6 @@
 FIRST, SECOND = f'0{input()}', f'0{input()}'
 N_FIRST, N_SECOND = len(FIRST), len(SECOND)
-dp = [[0] * (N_SECOND) for _ in range(N_FIRST)]
+dp = [0] * N_SECOND
 
 #   0 S E C O N D
 # 0 0 0 0 0 0 0 0
@@ -11,10 +11,11 @@ dp = [[0] * (N_SECOND) for _ in range(N_FIRST)]
 # T 0
 
 for i in range(1, N_FIRST):
+    count = 0
     for j in range(1, N_SECOND):
-        if FIRST[i] == SECOND[j]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) 
+        if count < dp[j]:
+            count = dp[j]
+        elif FIRST[i] == SECOND[j]:
+            dp[j] = count + 1
 
-print(dp[-1][-1])
+print(max(dp))
