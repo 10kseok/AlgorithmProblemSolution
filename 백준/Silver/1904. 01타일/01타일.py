651c8b66bf96c45cb94dfeb9329560a70698d635
추만석 <koesnam.dev@gmail.com>
2024-01-29 21:25:12 +0900

    [Silver III] Title: 01타일, Time: 168 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,8 +1,11 @@
 import sys
 
-a, b = 0, 1
-N = int(sys.stdin.readline())
-for _ in range(N):
-    a, b = b % 15746, (a + b) % 15746
-print(b)
+def main():
+    a, b = 0, 1
+    N = int(sys.stdin.readline())
+    for _ in range(N):
+        a, b = b % 15746, (a + b) % 15746
+    print(b)
+    
+main()
 
