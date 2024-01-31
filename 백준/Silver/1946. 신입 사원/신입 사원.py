b7300c1fef064a677bd2ad2664398750bc2b2b34
추만석 <koesnam.dev@gmail.com>
2024-01-31 22:43:15 +0900

    [Silver I] Title: 신입 사원, Time: 1624 ms, Memory: 35756 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -24,6 +24,28 @@ def solution():
 
         result.append(count)
     print(*result, sep="\n")
-    
+
+def solution2():
+    T = int(input())
+    result = []
+    for _ in range(T):
+        N = int(input())
+        new_recruits = [0] * (N + 1)
+
+        for _ in range(N):
+            paper, interview = map(int, input().split())
+            new_recruits[paper] = interview
+
+        count = 1
+        better_recruit = new_recruits[1]
+        for rank in range(2, N + 1):
+            if better_recruit > new_recruits[rank]:
+                better_recruit = new_recruits[rank]
+                count += 1
+
+        result.append(str(count))
+    print("\n".join(result))
+
 if __name__=="__main__":
-    solution()
+    # solution()
+    solution2()
