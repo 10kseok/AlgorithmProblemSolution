59a804f53969b1b8f5fa90f7bcecb4fcc7859278
koesnam <76582376+10kseok@users.noreply.github.com>
2024-02-15 12:34:52 +0900

    주유소 | 입력방식 변경으로 속도 개선
    백준 허브가 동작하지 않아 직접 커밋

--- 백준/Silver III/13305. 주유소/주유소.java
+++ 백준/Silver III/13305. 주유소/주유소.java
@@ -4,20 +4,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        reader.readLine();
+//        int[] distance = Arrays.stream(reader.readLine().split("\\s"))
+//                .mapToInt(Integer::parseInt)
+//                .toArray();
+//        int[] costsByCity = Arrays.stream(reader.readLine().split("\\s"))
+//                .mapToInt(Integer::parseInt)
+//                .toArray();
+//
+//        writer.write(String.valueOf(calculateMinimumOilCost(distance, costsByCity)));
+//        reader.close();
+//        writer.close();
+//    }
+//    private static long calculateMinimumOilCost(int[] distance, int[] costsByCity) {
+//        long minimumCost = costsByCity[0];
+//        long total = minimumCost * distance[0];
+//        for (int i = 1; i < distance.length; i++) {
+//            if (minimumCost > costsByCity[i]) minimumCost = costsByCity[i];
+//            total += minimumCost * distance[i];
+//        }
+//        return total;
+//    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         reader.readLine();
-        int[] distance = Arrays.stream(reader.readLine().split("\\s"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        int[] costsByCity = Arrays.stream(reader.readLine().split("\\s"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        int[] distance = splitAndReturn(new StringTokenizer(reader.readLine()));
+        int[] costsByCity = splitAndReturn(new StringTokenizer(reader.readLine()));
 
         writer.write(String.valueOf(calculateMinimumOilCost(distance, costsByCity)));
         reader.close();
@@ -32,5 +54,13 @@ class Main {
             total += minimumCost * distance[i];
         }
         return total;
+    }
+
+    private static int[] splitAndReturn(StringTokenizer tokenizer) {
+        int[] result = new int[tokenizer.countTokens()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+        return result;
     }
 }
