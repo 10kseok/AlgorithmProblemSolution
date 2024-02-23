6fc58f507886a69e437fa8df1dbc4a1d205f58da
추만석 <koesnam.dev@gmail.com>
2024-02-23 16:39:49 +0900

    [Gold III] Title: 아침 산책, Time: 1124 ms, Memory: 110192 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,36 +1,38 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
 OUTSIDE, INSIDE, PADDING = '0', '1', '0'
-
 def solution():
     N = int(input())
     place = PADDING + input()
     graph = [[] for _ in range(N + 1)]
+    total_cnt = 0
+    
     for _ in range(N - 1):
         start, end = map(int, input().split())
         graph[start].append(end)
         graph[end].append(start)
+        if (place[start] == INSIDE and place[end] == INSIDE): total_cnt += 2
     
-    global cnt
-    cnt = 0
-    def dfs(idx, visited, start_flag):
-        global cnt
-        if visited[idx]: return
-        if start_flag == 0 and place[idx] == INSIDE:
-            cnt += 1
-            return
-        
-        visited[idx] = True
-        for target in graph[idx]:
-            if not visited[target]:
-                dfs(target, visited, 0)
-                
-    for i in range(1, N + 1):
-        if place[i] == OUTSIDE: continue
-        dfs(i, [False] * (N + 1), 1)
+    def dfs(node, visited, inside_cnt):
+        visited[node] = True
+        for next_node in graph[node]:
+            if visited[next_node]: continue
+            if place[next_node] == INSIDE:
+                inside_cnt += 1
+                continue
+            if place[next_node] == OUTSIDE:
+                inside_cnt = dfs(next_node, visited, inside_cnt)
+        return inside_cnt
 
-    print(cnt)
+    visited = [False] * (N + 1)
+    for i in range(1, N + 1):
+        if not visited[i] and place[i] == OUTSIDE:
+            inside_cnt = dfs(i, visited, 0)
+            total_cnt += inside_cnt * (inside_cnt - 1)
+    
+    print(total_cnt)
     
 if __name__=="__main__":
     solution()
