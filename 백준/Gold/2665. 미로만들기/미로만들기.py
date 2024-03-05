bddb3fd4c92131cdfd48be2e9b25ee953f8788bc
추만석 <koesnam.dev@gmail.com>
2024-03-05 19:45:06 +0900

    [Gold IV] Title: 미로만들기, Time: 48 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,5 +1,5 @@
 import sys
-from collections import deque
+from heapq import heappush, heappop
 
 input = sys.stdin.readline
 DIRECTION = [(1, 0), (-1, 0), (0, 1), (0, -1)]
@@ -7,29 +7,21 @@ DIRECTION = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 def solution():
     board_size = int(input())
     board = [input() for _ in range(board_size)]
-    
-    visited = [[False] * board_size for _ in range(board_size)]
-    black_rooms = [[float('inf')] * board_size for _ in range(board_size)]
-    visited[0][0] = True
-    black_rooms[0][0] = 0
-    queue = deque([(0, 0)])
-    
+    dark_rooms = [[float('inf')] * board_size for _ in range(board_size)]
+
+    queue = [(0, 0, 0)]
     while queue:
-        row, col = queue.popleft()
-        visited[row][col] = True
+        dark_room, row, col = heappop(queue)
         for k in range(4):
             next_r, next_c = row + DIRECTION[k][0], col + DIRECTION[k][1]
             if next_r < 0 or next_r >= board_size or next_c < 0 or next_c >= board_size: continue
             # '0' -> 검은 방, '1' -> 흰 방
-            if board[next_r][next_c] == '0' and black_rooms[next_r][next_c] > black_rooms[row][col] + 1:
-                black_rooms[next_r][next_c] = black_rooms[row][col] + 1
-                queue.append((next_r, next_c))
-            elif board[next_r][next_c] == '1' and black_rooms[next_r][next_c] > black_rooms[row][col]:
-                black_rooms[next_r][next_c] = black_rooms[row][col]
-                queue.append((next_r, next_c))  
-            elif not visited[next_r][next_c]:
-                visited[next_r][next_c] = True
-                queue.append((next_r, next_c))
-    print(black_rooms[-1][-1])
+            if board[next_r][next_c] == '0' and dark_rooms[next_r][next_c] > dark_room + 1:
+                dark_rooms[next_r][next_c] = dark_room + 1
+                heappush(queue, (dark_room + 1, next_r, next_c))
+            elif board[next_r][next_c] == '1' and dark_rooms[next_r][next_c] > dark_room:
+                dark_rooms[next_r][next_c] = dark_room
+                heappush(queue, (dark_room, next_r, next_c))
+    print(dark_rooms[-1][-1])
 if __name__=="__main__":
-    solution() 
+    solution()
