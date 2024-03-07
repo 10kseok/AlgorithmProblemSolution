0fda7f1325029c4468e50dacd73d152afccc07d3
추만석 <koesnam.dev@gmail.com>
2024-03-07 11:00:31 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -5,14 +5,28 @@ MAX = 5000
 
 def solution():
     # 3 ~ 5000까지 필요한 봉지 최소 갯수 입력
-    N = int(input())
-    min_table = [MAX for _ in range(MAX + 1)]
-    min_table[3], min_table[5] = 1, 1
+    # dp 풀이
+    #N = int(input())
+    #min_table = [MAX for _ in range(MAX + 1)]
+    #min_table[3], min_table[5] = 1, 1
     # f(n) = min(f(n - 3), f(n - 5)) + 1
-    for i in range(6, N + 1):
-        compare_3bag, compare_5bag = i - 3, i - 5
-        min_table[i] = min(min_table[compare_3bag], min_table[compare_5bag]) + 1
-    print(min_table[N] if min_table[N] < MAX else -1)
+    #for i in range(6, N + 1):
+    #    compare_3bag, compare_5bag = i - 3, i - 5
+    #    min_table[i] = min(min_table[compare_3bag], min_table[compare_5bag]) + 1
+    #print(min_table[N] if min_table[N] < MAX else -1)
+    
+    # 그리디 풀이
+    N = int(input())
+    result = 0
+    while N >= 0:
+        if N % 5 == 0:
+            result += (N//5)
+            print(result)
+            break
+        N -= 3
+        result += 1
+    else:
+        print(-1)
 
 if __name__=="__main__":
     solution() 
