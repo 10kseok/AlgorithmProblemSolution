98bcab9347f76910088677498fb8f4a2c364156c
추만석 <koesnam.dev@gmail.com>
2024-03-11 14:28:48 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 488 ms, Memory: 62120 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def solution():
+    N, M, R = map(int, input().split())
+    graph = [[] for _ in range(N + 1)]
+    for _ in range(M):
+        start, end = map(int, input().split())
+        graph[start].append(end)
+        graph[end].append(start)
+    for nodes in graph:
+        nodes.sort()
+                
+    orders = [0 for _ in range(N + 1)]
+    visited = [False for _ in range(N + 1)]
+    
+    queue = deque([R])
+    visited[R] = True
+    order = 1
+    orders[R] = order
+    while queue:
+        node = queue.popleft()
+        for v in graph[node]:
+            if not visited[v]:
+                visited[v] = True
+                queue.append(v)
+                order += 1
+                orders[v] = order
+    
+    print(*orders[1:], sep="\n")
+
+if __name__=="__main__":
+    solution() 
