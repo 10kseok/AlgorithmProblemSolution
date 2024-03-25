4c01a865f3089e7e8355c6bc62db8612b30681a0
추만석 <koesnam.dev@gmail.com>
2024-03-25 15:32:53 +0900

    [Silver IV] Title: 수들의 합 2, Time: 40 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -6,17 +6,15 @@ def solution():
     subseq = list(map(int, input().split()))
     total, count, slow, fast = 0, 0, 0, 0
     
-    while True:
-        if total >= M:
-            total -= subseq[slow]
-            slow += 1
-        elif fast == N:
-            break
-        else:
-            total += subseq[fast]
+    total += subseq[slow]
+    for slow in range(N):
+        while fast < N - 1 and total < M:
             fast += 1
+            total += subseq[fast]
         if total == M:
             count += 1
+        total -= subseq[slow]
     print(count)
+    
 if __name__=="__main__":
     solution() 
