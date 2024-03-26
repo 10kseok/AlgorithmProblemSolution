f8f4be8eb5cda48303b21dace04e5586b04324ab
koesnam <76582376+10kseok@users.noreply.github.com>
2024-03-26 13:38:08 +0900

    올바른 배열 | 투포인터 풀이 추가

--- 백준/Silver/1337. 올바른 배열/올바른 배열.py
+++ 백준/Silver/1337. 올바른 배열/올바른 배열.py
@@ -4,16 +4,29 @@ input = sys.stdin.readline
 def solution():
     N = int(input())
     array = sorted([int(input()) for _ in range(N)])
-    min_needs = 4
+    min_needs, count, faster_idx = 4, 4, 0
     
-    for idx, num in enumerate(array):
-        nexts = [i for i in range(num, num + 5)]
+    for idx, slower in enumerate(array) :
         count = 4
-        for n_num in array[idx+1:idx+5]:
-            if n_num in nexts:
-                count -= 1
-        if count < min_needs:
+        faster_idx = idx + 1
+        while faster_idx < N and slower + 4 >= array[faster_idx] and count > 0:
+            faster_idx += 1
+            count -= 1
+        if min_needs > count:
             min_needs = count
     print(min_needs)
+    
+    # for idx, num in enumerate(array):
+    #     nexts = [range(num, num + 5)]
+    #     count = 4
+    #     for n_num in array[idx:idx+5]:
+    #         if n_num not in nexts:
+    #             count -= 1
+    #     if count < min_needs:
+    #         min_needs = count
+    # print(min_needs)
 if __name__=="__main__":
     solution() 
+
+
+
