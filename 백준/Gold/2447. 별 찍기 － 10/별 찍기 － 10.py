b2b216fb893267f728b22f19e6464086dd8316aa
추만석 <koesnam.dev@gmail.com>
2024-04-08 14:10:54 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 52 ms, Memory: 40548 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,12 +1,33 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
 
-def makeStars(n):
-    if n == 3:
-        star_list = ['***', '* *', '***']
-        return star_list
-    else:
-        new_star_list = [x * 3 for x in makeStars(n//3)] + [x + ' '*(n//3) + x for x in makeStars(n//3)] + [x * 3 for x in makeStars(n//3)]
+def solution():
+    N = int(input())
+    # result = [[' '] * N for _ in range(N)]
+    # def make_star(row, col, N):
+    #     if N == 1:
+    #         result[row][col] = '*'
+    #         return
+    #     sector = N // 3
+        
+    #     make_star(row, col, sector); make_star(row, col + sector, sector); make_star(row, col + sector * 2, sector)
+    #     make_star(row + sector, col, sector); make_star(row + sector, col + sector * 2, sector)
+    #     make_star(row + sector * 2, col, sector); make_star(row + sector * 2, col + sector, sector); make_star(row + sector * 2, col + sector * 2, sector)
+    # make_star(0, 0, N)
+    
+    # print('\n'.join([''.join(result[i]) for i in range(N)]))
+    
+    
+    def makeStars(n):
+        if n == 3:
+            star_list = ['***', '* *', '***']
+            return star_list
+        sector = makeStars(n//3)
+        new_star_list = [x * 3 for x in sector] + [x + ' ' * (n // 3) + x for x in sector] + [x * 3 for x in sector]
         return new_star_list
 
-for i in makeStars(n):
-    print(i)
+    print('\n'.join(makeStars(N)))
+if __name__=="__main__":
+    solution() 
+
