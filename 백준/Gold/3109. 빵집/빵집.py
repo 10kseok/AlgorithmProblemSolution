996f98437d49ed8578144986123c45db80a02581
추만석 <koesnam.dev@gmail.com>
2024-04-10 16:29:32 +0900

    [Gold II] Title: 빵집, Time: 1748 ms, Memory: 69884 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.py
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -1,15 +1,31 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 DIRECTIONS = [(-1, 1), (0, 1), (1, 1)]
 
-def solution():
-    R, C = map(int, input().split())
-    board = [input().strip() for _ in range(R)]
-    pipeline_cnt = 0
-    installed = [[False] * C for _ in range(R)]
+# def install(r, c):
+#     global board, R, C
+#     board[r][c] = 'x'
+#     if c == C - 1:
+#         return True
+
+#     for d in DIRECTIONS:
+#         nr, nc = r + d[0], c + d[1]
+#         if nr < 0 or R <= nr \
+#             or board[nr][nc] == 'x':
+#                 continue
+                
+#         if install(nr, nc):
+#             return True
+
+#     return False
     
+def solution():
+    global board, R, C
+    R, C = map(int, input().split())
+    pipeline_cnt = 0
+    
+    board = [list(input().strip()) for _ in range(R)]
     for r in range(R):
         install_stack = [(r, 0)]
         while install_stack:
@@ -17,15 +33,19 @@ def solution():
             if cur_c == C - 1:
                 pipeline_cnt += 1
                 break
-            installed[cur_r][cur_c] = True
+            board[cur_r][cur_c] = 'x'
             for d in DIRECTIONS[::-1]:
                 nr, nc = cur_r + d[0], cur_c + d[1]
-                if nr < 0 or R <= nr or nc < 0 or C <= nc \
-                    or board[nr][nc] == 'x' \
-                    or installed[nr][nc]:
+                if nr < 0 or R <= nr or board[nr][nc] == 'x':
                     continue
                 install_stack.append((nr, nc))
-        
+                
+    # use Recursive func
+    # board = [list(input().strip()) for _ in range(R)]
+    # for r in range(R):
+    #     if install(r, 0):
+    #         pipeline_cnt += 1
+            
     print(pipeline_cnt)
     
 if __name__=="__main__":
