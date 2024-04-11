7f79bc8d56fb7f66e86a63cf4a5498dc1f6fed54
추만석 <koesnam.dev@gmail.com>
2024-04-11 14:57:22 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -16,12 +16,11 @@ def solution():
     virus_cnt = 0
     def virus(src):
         global virus_cnt
-        if not visited[src]:
-            visited[src] = True
-            virus_cnt += 1
-            
+        visited[src] = True
+          
         for next_computer in graph[src]:
             if not visited[next_computer]:
+                virus_cnt += 1
                 virus(next_computer)
     
     virus(1)
