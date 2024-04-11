d444508a2ae3042b92da0ad1ff9bdcbd12e5b149
추만석 <koesnam.dev@gmail.com>
2024-04-11 14:55:08 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,52 +1,31 @@
 import sys
+input = sys.stdin.readline
 
-N_computer = int(input())
-N_pairs = int(input())
-
-graph = [[] for _ in range(N_computer + 1)]
-visited = [False] * (N_computer + 1)
-count = 0
-
-for _ in range(N_pairs):
-    c1, c2 = map(int, sys.stdin.readline().split())
-    graph[c1].append(c2)
-    graph[c2].append(c1)
-
-def virus_dfs(i):
-    global count
-    if len(graph[i]) == 0:
-        return
-    visited[i] = True
-    for j in graph[i]:
-        if not visited[j]:
-            count += 1
-            virus_dfs(j)
-
-def virus_stack(i):
-    global count
-
-    stack = graph[i][:]
-    while stack:
-        computer = stack.pop()
-        if not visited[computer]:
-            visited[computer] = True
-            count += 1
-            stack.extend(graph[computer])
-
-def virus_bfs(i):
-    global count
-
-    queue = graph[i][:]
-    while queue:
-        computer = queue.pop(0)
-        if not visited[computer]:
-            visited[computer] = True
-            count += 1
-            queue.extend(graph[computer])
-
-
-virus_dfs(1); print(count)
-# count = 0; visited = [False] * (N_computer + 1); visited[1] = True
-# virus_stack(1); print(count)
-# count = 0; visited = [False] * (N_computer + 1); visited[1] = True
-# virus_bfs(1); print(count)
+def solution():
+    global virus_cnt
+    computer_cnt = int(input())
+    pair_cnt = int(input())
+    graph = [[] for _ in range(computer_cnt + 1)]
+    for _ in range(pair_cnt):
+        src, dest = map(int, input().split())
+        graph[src].append(dest)
+        graph[dest].append(src)
+        
+    visited = [False] * (computer_cnt + 1)
+    visited[1] = True
+    virus_cnt = 0
+    def virus(src):
+        global virus_cnt
+        if not visited[src]:
+            visited[src] = True
+            virus_cnt += 1
+            
+        for next_computer in graph[src]:
+            if not visited[next_computer]:
+                virus(next_computer)
+    
+    virus(1)
+    print(virus_cnt)
+   
+if __name__=="__main__":
+    solution() 
