c1d6409fb4140e05c8d50d496e701af7a0a4f5e7
추만석 <koesnam.dev@gmail.com>
2024-04-18 00:52:40 +0900

    [Silver II] Title: 랜선 자르기, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -5,18 +5,14 @@ def solution():
     K, N = map(int, input().split())
     lans = [int(input()) for _ in range(K)]
     min_length, max_length = 1, max(lans)
-    last_cutting_length = 1
     while min_length <= max_length:
         cutting_length = (min_length + max_length) // 2
-        cnt = 0
-        for lan in lans:
-            cnt += lan // cutting_length
+        cnt = sum([lan // cutting_length for lan in lans])
         if cnt < N:
             max_length = cutting_length - 1
         else:
-            last_cutting_length = cutting_length
             min_length = cutting_length + 1
-    print(last_cutting_length)
-
+    print(max_length)
+    
 if __name__=="__main__":
     solution() 
