36a49f82d8de6d2589f7a7c157f20530f7a0acae
추만석 <koesnam.dev@gmail.com>
2024-05-03 12:31:18 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -0,0 +1,16 @@
+def solution(n):
+    MAX = 14
+    answer = 0
+    # ["", ""]
+    # 각 인덱스에 몇번 집어넣을지 고려
+    # DP? ok, DFS? ok
+    # 첫번째 괄호에 몇개의 괄호를 사용하는지에 따라 구해보자 -> DP
+    # 괄호는 열고 닫는 것밖에 없고, n이 14기에 크기가 크기않다. 올바른 괄호를 모두 구해보자 -> DFS
+    
+    # DP
+    dp = [0] * (MAX + 1)
+    dp[0] = 1; dp[1] = 1; dp[2] = 2; dp[3] = 5;
+    for i in range(4, n + 1):
+        dp[i] = sum([dp[j] * dp[(i-1) - j] for j in range(0, i)])
+    answer = dp[n]
+    return answer
