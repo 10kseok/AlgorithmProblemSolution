d01f52ba9cc6fc5bd6a74179e1152388486bde1d
추만석 <koesnam.dev@gmail.com>
2024-07-04 12:32:56 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -11,15 +11,20 @@ def solution():
         player = (int(level), name)
         available = False
         for room in rooms:
-            if player[0] - 10 <= room[0][0] <= player[0] + 10 and len(room) < m:
+            if len(room) >= m:
+                continue
+            if player[0] - 10 <= room[0][0] <= player[0] + 10:
                 available = True
                 room.append(player)
                 break
         if not available:
             rooms.append([player])
+    
+    answer = []
     for room in rooms:
         room.sort(key=lambda x: x[1])
-        print("Started!" if len(room) == m else "Waiting!")
-        print(*map(lambda x:f'{x[0]} {x[1]}', room), sep="\n")
+        answer.append("Started!" if len(room) == m else "Waiting!")
+        answer.extend(list(map(lambda x:f'{x[0]} {x[1]}', room)))
+    print("\n".join(answer))
 if __name__=="__main__":
     solution() 
