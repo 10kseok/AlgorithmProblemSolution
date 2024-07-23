e3748640ad90f2a7de74d8e0753f7efca7824439
추만석 <koesnam.dev@gmail.com>
2024-07-24 00:13:27 +0900

    [Gold IV] Title: 합이 0, Time: 6664 ms, Memory: 32456 KB -BaekjoonHub

---
+++ 백준/Gold/3151. 합이 0/합이 0.py
@@ -0,0 +1,18 @@
+from sys import stdin
+
+input = stdin.readline
+
+def solution(N, students):    
+    counter = [0] * 40_001
+    answer = 0
+    for i in range(N):
+        answer += counter[20_000 - students[i]]
+        for j in range(i):
+            counter[20_000 + students[i] + students[j]] += 1
+            
+    print(answer)
+
+if __name__=="__main__":
+    N = int(input())
+    students = list(map(int, input().split()))
+    solution(N, students)
