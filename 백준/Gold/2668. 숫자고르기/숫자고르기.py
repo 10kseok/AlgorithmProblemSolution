8eabdb2af0c1a493c480e8718122aa2c24050bce
추만석 <koesnam.dev@gmail.com>
2024-07-23 18:05:32 +0900

    [Gold V] Title: 숫자고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -3,22 +3,19 @@ from sys import stdin
 input = stdin.readline
 
 def solution(N, nums):    
-    visited = [False] * (N + 1)
     answer = []
-    def dfs(i, path):
-        if visited[i]:
-            return
+    def dfs(i, start):
         visited[i] = True
-        path.append(i)
-        if visited[nums[i]] and path[0] == nums[i]:
-            answer.extend(path)
+        _next = nums[i]
+        if not visited[_next]:
+            dfs(_next, start)
             return
-        dfs(nums[i], path)
-        if i not in answer:
-            visited[i] = False
+        if start == _next:
+            answer.append(_next)
             
     for i in range(1, N + 1):
-        dfs(i, [])
+        visited = [False] * (N + 1)
+        dfs(i, i)
     
     answer.sort()
     print(len(answer))
@@ -29,4 +26,3 @@ if __name__=="__main__":
     N = int(input())
     nums = [0] + [int(input()) for _ in range(N)]
     solution(N, nums)
-
