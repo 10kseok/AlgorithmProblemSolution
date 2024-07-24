3d780839d77f1b424d84f958eb8b2cdd6a652ee5
추만석 <koesnam.dev@gmail.com>
2024-07-24 16:12:04 +0900

    [Gold IV] Title: 합이 0, Time: 1840 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.py
+++ 백준/Gold/3151. 합이 0/합이 0.py
@@ -14,37 +14,71 @@ def solution(N, students):
     # print(answer)
     
     # Use Two-pointer
-    students.sort()
-    answer = 0
-    for i in range(N):
-        if students[i] > 0:
-            break
-        lower, upper = i + 1, N - 1
-        while lower < upper:
-            total = students[lower] + students[upper]
-            if total < -students[i]:
-                lower += 1
-            elif total > -students[i]:
-                upper -= 1
-            else:
-                if students[lower] == students[upper]:
-                    # nCr => nC2
-                    n = upper - lower
-                    answer += (n + 1) * n // 2
-                    break
-                else:
-                    # 중복 lower * 중복 upper
-                    dup_lower, dup_upper = 1, 1
-                    while students[lower] == students[lower + 1]:
-                        lower += 1
-                        dup_lower += 1
-                    while students[upper] == students[upper - 1]:
-                        upper -= 1
-                        dup_upper += 1
-                    answer += dup_lower * dup_upper
-                    upper -= 1 # 다른 조합도 있는지 확인하기 위해 (lower와 upper가 연속적인 수가 아닐 때 발생)
-    print(answer)
+    # students.sort()
+    # answer = 0
+    # for i in range(N):
+    #     if students[i] > 0:
+    #         break
+    #     lower, upper = i + 1, N - 1
+    #     while lower < upper:
+    #         total = students[lower] + students[upper]
+    #         if total < -students[i]:
+    #             lower += 1
+    #         elif total > -students[i]:
+    #             upper -= 1
+    #         else:
+    #             if students[lower] == students[upper]:
+    #                 # nCr => nC2
+    #                 n = upper - lower
+    #                 answer += (n + 1) * n // 2
+    #                 break
+    #             else:
+    #                 # 중복 lower * 중복 upper
+    #                 dup_lower, dup_upper = 1, 1
+    #                 while students[lower] == students[lower + 1]:
+    #                     lower += 1
+    #                     dup_lower += 1
+    #                 while students[upper] == students[upper - 1]:
+    #                     upper -= 1
+    #                     dup_upper += 1
+    #                 answer += dup_lower * dup_upper
+    #                 upper -= 1 # 다른 조합도 있는지 확인하기 위해 (lower와 upper가 연속적인 수가 아닐 때 발생)
+    # print(answer)
+    
+    # Use divided counter
+    answer = 0 
+    positive = [0] * 10001
+    negative = [0] * 10001
+    for student in students: 
+        if student < 0:
+            negative[abs(student)] += 1
+        else:
+            positive[student] += 1
             
+    def pick_two(cur, container):
+        local_total = 0
+        for i in range(cur // 2 + 1):
+            if i != cur - i:
+                local_total += container[i] * container[cur - i]
+        
+        if cur % 2 == 0:
+            middle_couple = container[cur // 2]
+            if middle_couple >= 2:
+                local_total += middle_couple * (middle_couple - 1) // 2
+
+        return local_total
+    
+    for i in range(1, 10001):
+        if positive[i]:
+            answer += pick_two(i, negative) * positive[i]
+        if negative[i]:
+            answer += pick_two(i, positive) * negative[i]
+
+    if positive[0] >= 3:
+        zero_val = positive[0]
+        answer += zero_val * (zero_val - 1) * (zero_val - 2) // 6
+    print(answer)
+    
 if __name__=="__main__":
     N = int(input())
     students = list(map(int, input().split()))
