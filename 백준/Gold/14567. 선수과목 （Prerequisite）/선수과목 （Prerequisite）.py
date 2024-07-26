90574c57932a237c4df2b6700c6cae36117e5786
추만석 <koesnam.dev@gmail.com>
2024-07-26 13:59:39 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 928 ms, Memory: 126216 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -3,13 +3,23 @@ from sys import stdin
 input = stdin.readline
 
 def solution(n, lectures):
-    lectures.sort()
-    planner = [1] * (n + 1)
+    # Use DP
+    # lectures.sort()
+    # planner = [1] * (n + 1)
+    # for A, B in lectures:
+    #     planner[B] = max(planner[B], planner[A] + 1)
+    # print(*planner[1:])
+    
+    # Use topology graph (no sort)
+    prerequisite = [1] * (n + 1)
+    topology = [[] for _ in range(n + 1)]
     for A, B in lectures:
-        planner[B] = max(planner[B], planner[A] + 1)
+        topology[B].append(A)
+    for i in range(1, n + 1):
+        if topology[i]:
+            prerequisite[i] = max([prerequisite[j] for j in topology[i]]) + 1
     
-    print(*planner[1:])
-    
+    print(*prerequisite[1:])
 if __name__=="__main__":
     N, M = map(int, input().split())
     lectures = [list(map(int, input().split())) for _ in range(M)]
