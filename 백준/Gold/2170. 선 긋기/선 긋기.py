9450aa74504f5c9de26621b78dfdd8961ac25b9b
추만석 <koesnam.dev@gmail.com>
2024-07-26 17:03:55 +0900

    [Gold V] Title: 선 긋기, Time: 4056 ms, Memory: 239600 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.py
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -2,22 +2,24 @@ from sys import stdin
 
 input = stdin.readline
 
-def solution(positions):
+def solution(n):
+    positions = [list(map(int, input().split())) for _ in range(n)]
     positions.sort()
     
     line_start = positions[0][0]
     line_end = positions[0][1]
     length = line_end - line_start
     for x, y in positions[1:]:
-        if x <= line_end < y:
+        if y <= line_end:
+            continue
+        if x <= line_end:
             length += y - line_end
             line_end = y
-        elif line_end < x:
+        else:
             line_start, line_end = x, y
             length += line_end - line_start
     print(length)
         
 if __name__=="__main__":
     N = int(input())
-    positions = [list(map(int, input().split())) for _ in range(N)]
-    solution(positions)
+    solution(N)
