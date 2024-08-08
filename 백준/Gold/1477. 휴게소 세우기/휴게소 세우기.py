f5a55857e1cc354253ba983a4ac54387f2a43196
추만석 <koesnam.dev@gmail.com>
2024-08-08 23:58:30 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+input = stdin.readline
+
+def solution(n, m, l):
+    # 0 <= n <= 50
+    rests = [0] + sorted(list(map(int, input().split()))) + [l] if n else [0] + [l]
+    n = len(rests)
+    
+    lower, upper = 1, l - 1
+    while lower <= upper:
+        gap = (lower + upper) // 2
+        cnt = 0
+        for i in range(1, n):
+            cnt += (rests[i] - rests[i - 1] - 1) // gap # 이미 있는 위치에 설치하지 않게 하려고 -1
+        if cnt <= m:
+            upper = gap - 1
+        else:
+            lower = gap + 1
+    print(lower)
+
+if __name__=="__main__":
+    N, M, L = map(int, input().split())
+    solution(N, M, L)
