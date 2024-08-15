d82d04ffe11d97ff5090b0dad5aa1a6fe702c710
추만석 <koesnam.dev@gmail.com>
2024-08-15 19:07:27 +0900

    [level 2] Title: 소수 찾기, Time: 39.61 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,22 +3,17 @@ import java.util.*;
 class Solution {
     private static Set<Integer> results = new HashSet();
     
-    private static void permutation(String buffer, String numbers, boolean[] visited) {
+    private static void permutation(String buffer, String numbers) {
         for (int j = 0; j < numbers.length(); j++) {
-            if (!visited[j]) {
-                visited[j] = true;
-                String merged = buffer + numbers.charAt(j);
-                results.add(Integer.valueOf(merged));
-                permutation(merged, numbers, Arrays.copyOf(visited, visited.length));
-                visited[j] = false;
-            }
+            String merged = buffer + numbers.charAt(j);
+            results.add(Integer.valueOf(merged));
+            permutation(merged, numbers.substring(0, j) + numbers.substring(j + 1, numbers.length()));
         }
     }
     
     private static boolean isPrimeNumber(int num) {
-        if (num < 2) {
+        if (num < 2)
             return false;
-        }
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0)
                 return false;
@@ -28,7 +23,7 @@ class Solution {
     public int solution(String numbers) {
         int answer = 0;
         // 나올 수 있는 숫자의 모든 경우를 구한다
-        permutation("", numbers, new boolean[numbers.length()]);
+        permutation("", numbers);
         // 각 경우가 소수면 answer++
         for (Integer s: results) {
             if (isPrimeNumber(s))
