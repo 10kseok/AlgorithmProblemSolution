4464555d744a4fc24a1a19924e186fe0572862da
추만석 <koesnam.dev@gmail.com>
2024-08-19 12:50:01 +0900

    [Silver III] Title: N과 M (3), Time: 1732 ms, Memory: 121896 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -7,14 +7,18 @@ def solution(n, m):
     results = []
     
     def choose(_nums, r, buffer):
-        if r == 0:
-            results.append(buffer)
-            return
+        # if r == 0:
+        #     results.append(buffer)
+        #     return
         
-        for i in range(len(_nums)):
-            buffer.append(_nums[i])
-            choose(_nums, r - 1, buffer[:])
-            buffer.pop()
+        # for i in range(len(_nums)):
+        #     buffer.append(_nums[i])
+        #     choose(_nums, r - 1, buffer[:])
+        #     buffer.pop()
+        
+        # Use product
+        from itertools import product
+        results.extend(list(product(nums, repeat=m)))
         
     choose(nums, m, [])
     for rst in results:
