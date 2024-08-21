0f60a20d1a1dd773a38ec68cb85f2229c19d4392
추만석 <koesnam.dev@gmail.com>
2024-08-21 12:34:13 +0900

    [Platinum IV] Title: 열혈강호, Time: 220 ms, Memory: 61932 KB -BaekjoonHub

--- 백준/Platinum/11375. 열혈강호/열혈강호.py
+++ 백준/Platinum/11375. 열혈강호/열혈강호.py
@@ -5,26 +5,27 @@ input = stdin.readline
 def solution(n, m):
     workers = [[]] + [list(map(int, input().split()))[1:] for _ in range(n)]
     job = [0] * (m + 1)
-    checked = [False] * (m + 1)
 
     def match(worker_idx):
+        for job_idx in workers[worker_idx]:
+            if not job[job_idx]:
+                job[job_idx] = worker_idx
+                return 1
         for job_idx in workers[worker_idx]:
             if checked[job_idx]:
                 continue
             checked[job_idx] = True
-            
-            if not job[job_idx] or match(job[job_idx]):
+            if match(job[job_idx]):
                 job[job_idx] = worker_idx
-                return True
-        return False
+                return 1
+        return 0
     
     answer = 0
     for i in range(1, n + 1):
-        if match(i):
-            answer += 1
+        checked = [False] * (m + 1)
+        answer += match(i)
         if answer == m:
             break
-        checked = [False] * (m + 1)
     print(answer)
     
 if __name__=="__main__":
