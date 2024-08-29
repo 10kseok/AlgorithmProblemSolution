4d62906946d18548fdb13e2ca7353fe6f3d66ea4
추만석 <koesnam.dev@gmail.com>
2024-08-29 18:45:45 +0900

    [Gold V] Title: 탑, Time: 388 ms, Memory: 99100 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,34 +1,20 @@
-n = int(input())
-tops = list(map(int, input().split()))
-answer = [0] * n
+from sys import stdin
 
-class Stack():
-    def __init__(self) -> None:
-        self.buffer = []
-    
-    def peek(self) -> tuple:
-        return self.buffer[-1]
-    
-    def is_empty(self) -> bool:
-        return len(self.buffer) == 0
-    
-    def push(self, value) -> None:
-        self.buffer.append(value)
-
-    def pop(self) -> tuple:
-        return self.buffer.pop()
-
-stack = Stack()
-stack.push((1, tops[0]))
-
-for i in range(1, n):
-    while not stack.is_empty():
-        if stack.peek()[1] > tops[i]:
-            high_top_idx = stack.peek()[0]
-            answer[i] = high_top_idx
-            break
-        else:
+input = stdin.readline
+        
+def solution(n):
+    # previous greater element
+    tops = list(map(int, input().split()))
+    received_top, stack = [0] * n, []
+    for i in range(n):
+        while stack and tops[i] > tops[stack[-1]]:
             stack.pop()
-    stack.push((i + 1, tops[i]))
-
-print(*answer)
+        
+        if stack:
+            received_top[i] = stack[-1] + 1
+        stack.append(i)
+    print(*received_top)
+    
+if __name__=="__main__":
+    N = int(input())
+    solution(N)
