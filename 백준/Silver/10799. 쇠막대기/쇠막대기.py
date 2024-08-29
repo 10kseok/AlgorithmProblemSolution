e95ba4721a369b33206f6e96d6de0af3d9fe21a3
추만석 <koesnam.dev@gmail.com>
2024-08-29 17:58:12 +0900

    [Silver II] Title: 쇠막대기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -3,15 +3,27 @@ from sys import stdin
 input = stdin.readline
         
 def solution(steel_sticks):
-    stack, cnt = [], 0
+    # Use stack
+    # stack, cnt = [], 0
+    # for i in range(len(steel_sticks)):
+    #     stick = steel_sticks[i]
+    #     if stick == '(':
+    #         stack.append(stick)
+    #         continue
+    #     stack.pop()
+    #     cnt += len(stack) if steel_sticks[i - 1] == '(' else 1
+    # print(cnt)
+    
+    # Just Use count
+    fragment, total = 0, 0
     for i in range(len(steel_sticks)):
         stick = steel_sticks[i]
         if stick == '(':
-            stack.append(stick)
+            fragment += 1
             continue
-        stack.pop()
-        cnt += len(stack) if steel_sticks[i - 1] == '(' else 1
-    print(cnt)
+        fragment -= 1
+        total += fragment if steel_sticks[i - 1] == '(' else 1
+    print(total)
     
 if __name__=="__main__":
     N = input().strip()
