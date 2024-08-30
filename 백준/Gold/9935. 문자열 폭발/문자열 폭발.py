b2ce7831562c342386c4d794423ca28dd4ba3aac
추만석 <koesnam.dev@gmail.com>
2024-08-30 11:45:32 +0900

    [Gold IV] Title: 문자열 폭발, Time: 484 ms, Memory: 42300 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -5,11 +5,25 @@ input = stdin.readline
 def solution(string, bomb):
     stack = []
     
+    # 1. append and pop
+    # for char in string:
+    #     stack.append(char)
+    #     if len(stack) >= len(bomb) and stack[-len(bomb):] == list(bomb):
+    #         for _ in range(len(bomb)):
+    #             stack.pop()
+    
+    # 2. append and reallocate => time over
+    # for char in string:
+    #     stack.append(char)
+    #     if len(stack) >= len(bomb) and stack[-len(bomb):] == list(bomb):
+    #         stack = stack[:-len(bomb)]
+    
+    # 3. append and remove once
     for char in string:
         stack.append(char)
         if len(stack) >= len(bomb) and stack[-len(bomb):] == list(bomb):
-            for _ in range(len(bomb)):
-                stack.pop()
+            del stack[-len(bomb):]
+
     
     if stack:
         print(''.join(stack))
