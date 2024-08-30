9b6cd570006dfb9efc72925e7060e2f346d7b335
추만석 <koesnam.dev@gmail.com>
2024-08-30 11:53:46 +0900

    [Gold IV] Title: 문자열 폭발, Time: 172 ms, Memory: 42300 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -19,12 +19,19 @@ def solution(string, bomb):
     #         stack = stack[:-len(bomb)]
     
     # 3. append and remove once
-    bomb = list(bomb)
+    # bomb = list(bomb)
+    # for char in string:
+    #     stack.append(char)
+    #     if len(stack) >= len(bomb) and stack[-len(bomb):] == bomb:
+    #         del stack[-len(bomb):]
+
+    # 4. check last character, use Time & Spatial Locality
+    bomb, bomb_size = list(bomb), len(bomb)
     for char in string:
         stack.append(char)
-        if len(stack) >= len(bomb) and stack[-len(bomb):] == bomb:
-            del stack[-len(bomb):]
-
+        if stack[-1] == bomb[-1] and stack[-bomb_size:] == bomb:
+            del stack[-bomb_size:]
+    
     if stack:
         print(''.join(stack))
     else:
