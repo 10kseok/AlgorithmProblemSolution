c5ee38356ca673fe62449ecedeb9eb3b7a07df1b
추만석 <koesnam.dev@gmail.com>
2024-09-03 00:57:39 +0900

    [Silver II] Title: A → B, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,29 +1,58 @@
 from sys import stdin
-from collections import deque
 
 input = stdin.readline
         
 def solution(a, b):
-    queue = deque([b])
-    cnt = 0
-    while queue:
-        num = queue.popleft()
+    # Use BFS
+    # from collections import deque
+    # queue = deque([b])
+    # cnt = 0
+    # while queue:
+    #     num = queue.popleft()
+    #     if num == a:
+    #         print(cnt + 1)
+    #         return
+    #     elif num < a:
+    #         continue
+        
+    #     if num % 2 == 0:
+    #         converted = num // 2
+    #     elif num % 10 == 1:
+    #         converted = (num - 1) // 10
+    #     else:
+    #         continue
+    #     queue.append(converted)
+    #     cnt += 1
+    # print(-1)
+    
+    # Use DFS
+    def dfs(num, cnt):
         if num == a:
             print(cnt + 1)
             return
         elif num < a:
-            continue
+            print(-1)
+            return
         
         if num % 2 == 0:
-            converted = num // 2
+            dfs(num // 2, cnt + 1)
         elif num % 10 == 1:
-            converted = (num - 1) // 10
+            dfs((num - 1) // 10, cnt + 1)
         else:
-            continue
-        queue.append(converted)
-        cnt += 1
-    print(-1)
-        
+            print(-1)
+    dfs(b, 0)
 if __name__=="__main__":
     A, B = map(int, input().split())
     solution(A, B)
+    
+# 3 4
+# => -1
+
+# 2 11
+# => -1
+
+# 2 182
+# => -1
+
+# 2 23
+# => -1
