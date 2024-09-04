5c1cce41ecdf2472585132411e5ffe8d7185cf7c
추만석 <koesnam.dev@gmail.com>
2024-09-04 23:17:19 +0900

    [Gold V] Title: 강의실 배정, Time: 164 ms, Memory: 46580 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,5 +1,5 @@
 from sys import stdin
-from heapq import heappush, heappop, heapify
+# from heapq import heappush, heappop, heapify
 
 input = stdin.readline
 
@@ -22,20 +22,37 @@ def solution(n):
     # print(len(rooms))
     
     # Use One Priority Queue
-    lectures = [tuple(map(int, input().split())) for _ in range(n)]
-    rooms = []
+    # lectures = [tuple(map(int, input().split())) for _ in range(n)]
+    # rooms = []
     
-    lectures.sort()
-    for start, end in lectures:
-        if not rooms:
-            rooms.append(end)
-            continue
-        if rooms and rooms[0] <= start:
-            heappop(rooms)
-        heappush(rooms, end)
+    # lectures.sort()
+    # for start, end in lectures:
+    #     if not rooms:
+    #         rooms.append(end)
+    #         continue
+    #     if rooms and rooms[0] <= start:
+    #         heappop(rooms)
+    #     heappush(rooms, end)
         
-    print(len(rooms))
+    # print(len(rooms))
     
+    # Use Cursor
+    starts, ends = [], []
+    for _ in range(n):
+        s, e = map(int, input().split())
+        starts.append(s)
+        ends.append(e)
+        
+    starts.sort()
+    ends.sort()
+    
+    cursor, rooms = 0, n
+    for s in starts:
+        if ends[cursor] <= s:
+            rooms -= 1
+            cursor += 1
+            
+    print(rooms)
 if __name__=="__main__":
     n = int(input())
-    solution(n)
+    solution(n)    
