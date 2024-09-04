0381185706384ad7e2ddf9d6a1bfac32a778d637
추만석 <koesnam.dev@gmail.com>
2024-09-04 23:08:28 +0900

    [Gold V] Title: 강의실 배정, Time: 240 ms, Memory: 61160 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -4,20 +4,36 @@ from heapq import heappush, heappop, heapify
 input = stdin.readline
 
 def solution(n):
+    # Use Two Priority Queue
+    # lectures = [tuple(map(int, input().split())) for _ in range(n)]
+    # rooms = []
+    
+    # heapify(lectures)
+    # while lectures:
+    #     if not rooms:
+    #         rooms.append(heappop(lectures)[1])
+    #         continue
+    #     start, end = heappop(lectures)
+        
+    #     if rooms and rooms[0] <= start:
+    #         heappop(rooms)
+    #     heappush(rooms, end)
+            
+    # print(len(rooms))
+    
+    # Use One Priority Queue
     lectures = [tuple(map(int, input().split())) for _ in range(n)]
     rooms = []
     
-    heapify(lectures)
-    while lectures:
+    lectures.sort()
+    for start, end in lectures:
         if not rooms:
-            rooms.append(heappop(lectures)[1])
+            rooms.append(end)
             continue
-        start, end = heappop(lectures)
-        
         if rooms and rooms[0] <= start:
             heappop(rooms)
         heappush(rooms, end)
-            
+        
     print(len(rooms))
     
 if __name__=="__main__":
