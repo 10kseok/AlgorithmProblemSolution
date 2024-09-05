598bf42f6859159d1d6aecd588c91f7a8c7f11d6
추만석 <koesnam.dev@gmail.com>
2024-09-05 17:41:23 +0900

    [Silver II] Title: 한 줄로 서기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -3,19 +3,34 @@ from sys import stdin
 input = stdin.readline
 
 def solution(n):
+    # Default Zero
+    # smaller_cnts = [0] + list(map(int, input().split()))
+    # answer = [0] * (n + 1)
+    
+    # for i in range(1, n + 1):
+    #     order = smaller_cnts[i]
+    #     temp_cnt = 0
+    #     for j in range(1, n + 1):
+    #         if answer[j] == 0 and temp_cnt == order:
+    #             answer[j] = i
+    #             break
+    #         elif temp_cnt < order and answer[j] == 0 or answer[j] > i:
+    #             temp_cnt += 1
+            
+    # Default Max
     smaller_cnts = [0] + list(map(int, input().split()))
-    answer = [0] * (n + 1)
+    answer = [11] * (n + 1)
     
     for i in range(1, n + 1):
         order = smaller_cnts[i]
         temp_cnt = 0
         for j in range(1, n + 1):
-            if answer[j] == 0 and temp_cnt == order:
-                answer[j] = i
-                break
-            elif temp_cnt < order and answer[j] == 0 or answer[j] > i:
+            if i < answer[j]:
+                if temp_cnt == order:
+                    answer[j] = i
+                    break
                 temp_cnt += 1
-            
+    
     print(*answer[1:])
 if __name__=="__main__":
     n = int(input())
