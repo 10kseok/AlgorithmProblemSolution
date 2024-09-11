4a69ea2db7038621206bf79bb655637ce718d334
추만석 <koesnam.dev@gmail.com>
2024-09-11 17:23:27 +0900

    [level 3] Title: 순위, Time: 10.70 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,64 +1,38 @@
 import java.util.*;
 
 class Solution {
-    private static int N;
-    private static List<Integer>[] winnerResult;
-    private static List<Integer>[] loserResult;
-    
     public int solution(int n, int[][] results) {
-        Solution.N = n;
-        Solution.winnerResult = new List[n + 1]; // 자신이 이긴 결과
-        Solution.loserResult = new List[n + 1]; // 자신이 졌던 결과
+        boolean[][] matchingBoard = new boolean[n + 1][n + 1];
         
-        for (int i = 1; i < n + 1; i++) {
-            winnerResult[i] = new ArrayList();
-            loserResult[i] = new ArrayList();
-        }
-            
         for (int[] result : results) {
             int winner = result[0], loser = result[1];
-            winnerResult[winner].add(loser);
-            loserResult[loser].add(winner);
+            matchingBoard[winner][loser] = true;
+        }
+        
+        for (int i = 1; i < n + 1; i++)
+            matchingBoard[i][i] = true;
+        
+        for (int stepover = 1; stepover < n + 1; stepover++) {
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < n + 1; j++) {
+                    if (matchingBoard[i][stepover] && matchingBoard[stepover][j])
+                        matchingBoard[i][j] = true;
+                }
+            }
         }
         
         int answer = 0;
         for (int i = 1; i < n + 1; i++) {
-            int result = countAllResult(i);
-            System.out.println(i + " |  result : " + result);
-            if (countAllResult(i) == n)
+            boolean[] player = matchingBoard[i];
+            int totalResult = 0;
+            for (int j = 1; j < n + 1; j++) {
+                if (matchingBoard[i][j] || matchingBoard[j][i])
+                    totalResult++;
+            }
+            if (totalResult == n)
                 answer++;
         }
+        
         return answer;
-    }
-    
-    private static int countAllResult(int player) {
-        int count = 1;
-        Queue<Integer> q = new LinkedList();
-        q.offer(player);
-        boolean[] visited = new boolean[N + 1];
-        
-        while (!q.isEmpty()) {
-            int p = q.poll();
-            for (int loser : winnerResult[p]) {
-                if (!visited[loser]) {
-                    visited[loser] = true;
-                    q.offer(loser);
-                    count++;
-                }
-            }
-        }
-        
-        q.offer(player);
-        while (!q.isEmpty()) {
-            int p = q.poll();
-            for (int winner : loserResult[p]) {
-                if (!visited[winner]) {
-                    visited[winner] = true;
-                    q.offer(winner);
-                    count++;
-                }
-            }
-        }
-        return count;
     }
 }
