ffc76a335a9ef7e7c77919c6ef66b69c3bcc13d7
추만석 <koesnam.dev@gmail.com>
2024-09-30 23:13:04 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1204 ms, Memory: 142104 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -4,23 +4,16 @@ input = stdin.readline
 
 def solution(m, n):
     snacks = list(map(int, input().split()))
-    def can_divide(snack_length):
-        if not snack_length:
-            return False
-        cnt = 0
-        for snack in snacks:
-            cnt += snack // snack_length
-        return cnt >= m
-    
+
     lower, upper = 1, max(snacks)
     answer = 0
     while lower <= upper:
-        mid = (lower + upper) >> 1
-        if can_divide(mid):
-            answer = mid
-            lower = mid + 1
+        length = (lower + upper) >> 1
+        if sum([snack // length for snack in snacks]) >= m:
+            answer = length
+            lower = length + 1
         else:
-            upper = mid - 1
+            upper = length - 1
     print(answer)
 
 if __name__=="__main__":
