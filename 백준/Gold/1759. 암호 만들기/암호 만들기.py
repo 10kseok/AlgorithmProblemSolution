44ff17925645b1950bf501d94306c82ef6d59868
추만석 <koesnam.dev@gmail.com>
2024-10-04 12:47:04 +0900

    [Gold V] Title: 암호 만들기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -4,19 +4,19 @@ input = stdin.readline
 
 def solution(l, c):
     possible_characters = list(set(input().split()))
-    possible_characters.sort()
-    c = len(possible_characters)
+    possible_characters.sort(); c = len(possible_characters)
+    vowels = set('aeiou')
     def find_password(chars: list, buffer: str, vowel: int, consonant: int):
         if len(buffer) == l and vowel >= 1 and consonant >= 2:
             answers.append(buffer)
             return
         for i in range(len(chars)):
-            is_vowel = chars[i] in ['a', 'e', 'i', 'o', 'u']
+            is_vowel = chars[i] in vowels
             find_password(chars[i + 1:], buffer + chars[i], vowel + int(is_vowel), consonant + int(not is_vowel))
     
     answers = []
     for i in range(c - l + 1):
-        is_vowel = possible_characters[i] in ['a', 'e', 'i', 'o', 'u']
+        is_vowel = possible_characters[i] in vowels
         find_password(possible_characters[i + 1:], possible_characters[i], int(is_vowel), int(not is_vowel))
     
     print('\n'.join(answers))
