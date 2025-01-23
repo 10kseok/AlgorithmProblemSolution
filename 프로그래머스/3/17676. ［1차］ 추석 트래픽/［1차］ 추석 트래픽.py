8a1869a470c3bf903f593de33fac67541be6581c
추만석 <koesnam.dev@gmail.com>
2025-01-23 17:31:38 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 3467.06 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.py
@@ -0,0 +1,36 @@
+def solution(lines):
+    answer = 0
+    
+    def get_time_milis(s: str) -> int:
+        """
+        ex)
+        s = 01:00:04.002
+        """
+        hours, minutes, seconds = s.split(":")
+        seconds, miliseconds = map(int, seconds.split("."))
+        return (int(hours) * 3600 + int(minutes) * 60 + seconds) * 1000 + miliseconds
+    
+    def get_start_time_milis(s: str, t: str) -> int:
+        """
+        ex)
+        s = 01:00:04.002
+        t = 2.0s
+        """
+        t_miliseconds = float(t[:-1]) * 1000
+        return get_time_milis(s) - t_miliseconds + 1
+    
+    max_throughput = 0
+    
+    lines_s_t_only = [line.split()[1:] for line in lines]
+    for i in range(len(lines_s_t_only)):
+        s, t = lines_s_t_only[i]
+        end_time_milis = get_time_milis(s) + 999
+        throughput = 1
+        for j in range(i + 1, len(lines_s_t_only)):
+            next_s, next_t = lines_s_t_only[j]
+            if get_start_time_milis(next_s, next_t) <= end_time_milis:
+                throughput += 1
+        if max_throughput < throughput:
+            max_throughput = throughput
+        
+    return max_throughput
