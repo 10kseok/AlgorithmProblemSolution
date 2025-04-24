abb5d98ff3ab6477e5ca325a3d4ba6607702fa10
koesnam <76582376+10kseok@users.noreply.github.com>
2025-04-25 00:46:51 +0900

    Remove binary addition solution and related README files

--- 0067-add-binary/0067-add-binary.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a, base=2) + int(b, base=2))[2:]
-        
--- 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def areOccurrencesEqual(self, s: str) -> bool:
-        from collections import defaultdict
-        count_dict = defaultdict(int)
-        for letter in s:
-            count_dict[letter] += 1
-        
-        return len(set(count_dict.values())) == 1
-        
